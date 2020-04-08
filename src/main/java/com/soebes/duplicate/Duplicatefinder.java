98456181fe20ec7eb9cbd2d86a4508eb9e350a1f
Karl Heinz Marbaise <kama@soebes.de>
2020-04-09 00:10:18 +0200

    Refactored.

--- src/main/java/com/soebes/duplicate/Duplicatefinder.java
+++ src/main/java/com/soebes/duplicate/Duplicatefinder.java
@@ -12,8 +12,17 @@ import java.util.stream.Collectors;
 
 public class Duplicatefinder {
   
-  private static final Function<Path, ChecksumForFileResult> checksumForFile = p ->
-    new CheckSum().forFile(p);
+  private static final Function<Path, ChecksumForFileResult> checksumForFile =
+    p -> new CheckSum().forFile(p);
+  
+  private static String formatting(Long readBytes) {
+    StringBuilder sb = new StringBuilder();
+    sb.append(" (");
+    
+    sb.append(String.format(Locale.GERMANY, "%,d", readBytes));
+    sb.append(" bytes.)");
+    return sb.toString();
+  }
   
   public static void main(String[] args) {
     
@@ -22,33 +31,31 @@ public class Duplicatefinder {
       List<Path> fileCollection = Files.list(Paths.get(args[0]))
         .filter(Files::isRegularFile)
         .collect(Collectors.toList());
-  
+      
       List<ChecksumForFileResult> checkSumResults = fileCollection.parallelStream()
         .map(checksumForFile)
         .collect(Collectors.toList());
       
       checkSumResults.stream().forEach(item -> {
         System.out.print(Convert.toHex(item.getDigest()));
-        System.out.print(" (" + String.format(Locale.GERMANY, "%,d",
-          item
-            .getReadBytes()) + " bytes.)");
+        System.out.print(formatting(item.getReadBytes()));
         System.out.println(" " + item.getFileName());
       });
-  
+      
       Map<ByteArrayWrapper, List<ChecksumForFileResult>> collect = checkSumResults.stream()
         .collect(Collectors.groupingBy(s -> s.getDigest()))
         .entrySet()
         .stream()
         .filter(s -> s.getValue().size() > 1)
         .collect(Collectors.toMap(k -> k.getKey(), v -> v.getValue()));
-  
+      
       System.out.println("Number of duplicates:" + collect.size());
       for (Map.Entry<ByteArrayWrapper, List<ChecksumForFileResult>> element : collect.entrySet()) {
         System.out.println("CheckSum: " + Convert.toHex(element.getKey()));
         
         for (ChecksumForFileResult item : element.getValue()) {
           System.out.print("  " + item.getFileName() + " (");
-          System.out.println(" (" + String.format(Locale.GERMANY, "%,d", item.getReadBytes()) + " bytes.)");
+          System.out.println(formatting(item.getReadBytes()));
         }
         
       }
