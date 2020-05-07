72284677569fdfeab5a67e7bcb0293afa33ffef1
Karl Heinz Marbaise <kama@soebes.de>
2020-05-07 23:22:37 +0200

    Updated.

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-~/tools/jdk-11.0.7+10/Contents/Home/bin/java -cp target/duplicate-finder-1.0-SNAPSHOT.jar com.soebes.duplicate.Duplicatefinder /Users/khmarbaise/Bilder/
+~/tools/jdk-11.0.7+10/Contents/Home/bin/java -jar target/duplicate-finder-1.0-SNAPSHOT.jar /Users/khmarbaise/Bilder/
