5427d8bc826e1d50ce930c81ae0e4ce86d359ca4
Karl Heinz Marbaise <kama@soebes.de>
2022-06-27 19:12:47 +0200

    Refactored.

--- src/main/java/com/soebes/duplicate/DuplicateFinder.java
+++ src/main/java/com/soebes/duplicate/DuplicateFinder.java
@@ -1,19 +1,29 @@
 package com.soebes.duplicate;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.toMap;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
 import java.util.Locale;
 import java.util.Map.Entry;
 import java.util.function.Function;
 
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toMap;
+
 class DuplicateFinder {
 
-  private static final Function<Path, ChecksumForFileResult> checksumForFile = CheckSum::forFile;
+  static Function<Path, ChecksumForFileResult> forFile = path -> {
+    try {
+      ChecksumResult checksumResult = new CalculateChecksum().forFile(path.toFile());
+      return new ChecksumForFileResult(checksumResult.digest(), path.getFileName()
+          .toString(), checksumResult.readBytes());
+    } catch (IOException | NoSuchAlgorithmException e) {
+      //Translate to RuntimeException.
+      throw new RuntimeException(e.getClass().getName(), e);
+    }
+  };
 
   private static String formatting(Long readBytes) {
     return " (" + String.format(Locale.GERMANY, "%,d", readBytes) + " bytes.)";
@@ -25,7 +35,7 @@ class DuplicateFinder {
 
       var fileCollection = Files.list(Paths.get(args[0])).filter(Files::isRegularFile).toList();
 
-      var checkSumResults = fileCollection.parallelStream().map(checksumForFile).toList();
+      var checkSumResults = fileCollection.parallelStream().map(forFile).toList();
 
       System.out.println("Total of found files:: " + checkSumResults.size());
       checkSumResults.forEach(item -> {
