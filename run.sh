c3e8399a743cbdfd933c4056bcfc79bfd9521c2b
Iyad Al-Sahwi <iyad@al-sahwi.de>
2024-04-12 13:03:09 +0200

    allow to enter a path

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-java -jar target/duplicate-finder.jar /Users/khm/Bilder/
+echo "Enter an image path to find duplicates in (e.g. /home/username/images):"
+read path
+
+if [ ! -d "$path" ]; then
+    echo "Error: Directory '$path' does not exist."
+    exit 1
+fi
+
+java -jar target/duplicate-finder.jar "$path"
